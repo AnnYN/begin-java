70c31d2ccdc257a5655abefc7e7bde8de800719d
yanan <liuyanan2017@126.com>
2019-04-30 19:11:42 +0800

    java

---
+++ src/main/java/io/yanan/PrintMatrix.java
@@ -0,0 +1,21 @@
+//package com.jdojo.statement;
+//package yanan.io;
+package io.yanan;
+public class PrintMatrix {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print(i + "" + j);
+                // Print a tab, except for the last number in a row
+                if (j < 3) {
+                    System.out.print("\t");
+                }
+            }
+            // Print a new line, except after the last line
+            if (i < 3) {
+                System.out.println();
+            }
+        }
+    }
+}
+
